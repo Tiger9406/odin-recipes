d578b61f39efa5b61c17c95d57c6c4dc6574ffef
Tiger <xcao2005@gmail.com>
2023-11-14 23:16:44 -0500

    completed tasks of creating and utilizing functions in js

---
+++ recipes/javascript.js
@@ -0,0 +1,18 @@
+console.log(23+97);
+var summation=1+2+3+4+5+6;
+console.log((3+6+9)/77);
+
+let a=10;
+console.log(a);
+let b=7*a;
+console.log(b);
+
+let MAX=57;
+let actual=MAX-13;
+let percentage=actual/MAX;
+
+const dawg="a";
+const greeting=`Hello, ${dawg}`;
+console.log(greeting);
+
+const joint = `${dawg} ${greeting}`;
